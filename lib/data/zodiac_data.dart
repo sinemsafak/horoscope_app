fc7e1deb04396f8027ed2fda4aa3c6fbbfd9c2a2
Sinem Şafak <safaksm14@gmail.com>
2026-01-30 17:34:51 +0300

    Burç Verileri

---
+++ lib/data/zodiac_data.dart
@@ -0,0 +1,16 @@
+import '../models/zodiac.dart';
+
+List<Zodiac> zodiacList = [
+  Zodiac(
+    name: "Koç",
+    date: "21 Mart - 20 Nisan",
+    description: "Koç burcu liderdir, cesurdur, enerjiktir...",
+    image: "assets/images/koc.png",
+  ),
+  Zodiac(
+    name: "Boğa",
+    date: "21 Nisan - 21 Mayıs",
+    description: "Boğa burcu sabırlıdır, güvenilirdir...",
+    image: "assets/images/boga.png",
+  ),
+];
