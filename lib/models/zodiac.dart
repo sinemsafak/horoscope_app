a57b8b3eb86a89ccd2f7f50a86cce31d777595f1
Sinem Şafak <safaksm14@gmail.com>
2026-01-30 17:34:09 +0300

    Add Zodiac data model

---
+++ lib/models/zodiac.dart
@@ -0,0 +1,13 @@
+class Zodiac {
+  final String name;
+  final String date;
+  final String description;
+  final String image;
+
+  Zodiac({
+    required this.name,
+    required this.date,
+    required this.description,
+    required this.image,
+  });
+}
