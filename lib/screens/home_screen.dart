dd64febb680db510d805fbc9402df1aef5ba77ba
Sinem Şafak <safaksm14@gmail.com>
2026-01-30 17:35:37 +0300

    Ana Ekran

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Burçlar")),
+      body: ListView.builder(
+        itemCount: zodiacList.length,
+        itemBuilder: (context, index) {
+          final zodiac = zodiacList[index];
+
+          return ListTile(
+            leading: Image.asset(zodiac.image, width: 40),
+            title: Text(zodiac.name),
+            subtitle: Text(zodiac.date),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetailScreen(zodiac: zodiac),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
